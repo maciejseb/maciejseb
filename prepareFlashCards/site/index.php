e0b3340a3b9e6afc4d8b12abfd7b5eda090645b1
maciejseb <61247848+maciejseb@users.noreply.github.com>
2023-04-15 17:19:59 +0200

    Add files via upload

--- prepareFlashCards/site/index.php
+++ prepareFlashCards/site/index.php
@@ -1,1 +1,11 @@
 
+<!DOCTYPE html>
+<html>
+<body style="font-size: 30px">
+<?php
+echo "strona w budowie <br>";
+?>
+<a href = 'https://maciejseb.pl/die-fima1.php'>die firma1</a><br>
+<a href = 'https://maciejseb.pl/pc-welt1.php'>pc welt1</a><br>
+</body>
+</html>
